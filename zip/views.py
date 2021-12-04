3fdd318ec394eaffa9d48961d89fd9fff22a33f0
masa <masa@gmail.com>
2021-12-05 00:32:17 +0900

    Added views to create/edit blog post inside the site.

--- zip/views.py
+++ zip/views.py
@@ -1,4 +1,72 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
+from django.http import HttpResponse
+import requests
+import json
+json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+'["foo", {"bar": ["baz", null, 1.0, 2]}]'
+
+
 
 def post_list(request):
-    return render(request, 'zip/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'zip/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'zip/post_detail.html', {'post': post})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            # url = 'https://api.zipaddress.net/?zipcode=1420063'
+            url = 'https://api.zipaddress.net/?zipcode={}'.format(post.title)
+            res = requests.get(url)
+            data = res.json()
+            post.text = data['data']['fullAddress']
+            return HttpResponse(post.text)
+            # return redirect('post_detail', pk=post.pk)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'zip/post_edit.html', {'form': form})
+
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             url = 'https://api.zipaddress.net/?zipcode={}'.format(post.title)
+#             # url = 'https://api.zipaddress.net/?zipcode=1420063'
+#             res = requests.get(url)
+#             data = res.json()
+#             post.text = data['data']['fullAddress']
+#             # post.published_date = timezone.now()
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'zip/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'zip/post_edit.html', {'form': form})
+
